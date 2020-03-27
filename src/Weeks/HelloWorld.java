2aa5969a853d311468775b2d22121ea9ee88bff0
tom_glb <goodMorning_pro@atguigu.com>
2020-03-27 16:28:02 +0800

    hot fix

--- src/Weeks/HelloWorld.java
+++ src/Weeks/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("no 2");
         System.out.println("3");
+        System.out.println("hot fix");
     }
     //开发人员写的
 }
